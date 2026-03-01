d5c9d9b715115f6c059c5a0f739afac9f8a11bd8
Who cares <Thakurtejas567@gmail.com>
2026-03-01 21:26:22 +0530

    Update hellocontroller.java

--- First week/My First WebApp/src-java/hellocontroller.java
+++ First week/My First WebApp/src-java/hellocontroller.java
@@ -1,11 +1,20 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<module type="JAVA_MODULE" version="4">
-  <component name="NewModuleRootManager" inherit-compiler-output="true">
-    <exclude-output />
-    <content url="file://$MODULE_DIR$">
-      <sourceFolder url="file://$MODULE_DIR$/src" isTestSource="false" />
-    </content>
-    <orderEntry type="inheritedJdk" />
-    <orderEntry type="sourceFolder" forTests="false" />
-  </component>
-</module>
+package com.first_day.truing_spring_boot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+
+public class hellocontroller {
+
+    // " say-hello" => "heyz dude `~"
+
+    //say-he llo
+    //http://loclhost:8080/say-hello
+@RequestMapping("say-hello")
+@ResponseBody
+    public String sayhello(){
+        return " heyz m   ~ ";
+    }
+}
